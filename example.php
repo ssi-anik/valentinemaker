40cb8483e0e58c388c737cf7d0bc45ac82d7b5c4
Sirajul <sirajul.islam.anik@gmail.com>
2014-01-31 12:49:49 +0600

    inserting example

--- example.php
+++ example.php
@@ -1,13 +1,13 @@
 <?php
     $request_url ="https://graph.facebook.com/" . $_SESSION['uid1']. "?fields=picture.width(140).height(120),first_name";
     $requests = file_get_contents($request_url);
-    print_r($request_url);
     $data = json_decode($requests);
+    print_r($data);
     $a = $data->picture->data->url;
     $_SESSION['uf1'] = $data->first_name;
     $request_url ="https://graph.facebook.com/" . $_SESSION['uid2']. "?fields=picture.width(140).height(120),first_name";
-    print_r($request_url);
     $requests = file_get_contents($request_url);
+    print_r($data);
     $data = json_decode($requests);
     $b = $data->picture->data->url;
     $_SESSION['uf2'] = $data->first_name;
