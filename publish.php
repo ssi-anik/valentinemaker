5755c4b8288206b5592ac79768cc9a1d73416e67
Sirajul <sirajul.islam.anik@gmail.com>
2014-02-01 02:11:41 +0600

    updating source

--- publish.php
+++ publish.php
@@ -9,6 +9,7 @@
     $user = $facebook->getUser();
     echo $user;
     echo "<br/>".$_GET['photo'];
+    var_dump(strpos($_SESSION['uid1'], $_GET['photo']));
     if($user && isset($_GET['photo']) && strpos($_SESSION['uid1'], $_GET['photo'])!==False){
         try{
             $facebook->setFileUploadSupport(true);
