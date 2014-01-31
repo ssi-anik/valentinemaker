efabd4e0c01395a97cb402d9e56858082f7c9570
Sirajul <sirajul.islam.anik@gmail.com>
2014-01-31 12:34:16 +0600

    update

--- imagemaker.php
+++ imagemaker.php
@@ -41,32 +41,36 @@
     );
     $user = $facebook->getUser();
     if($user ){
-        $request_url ="https://graph.facebook.com/" . $_SESSION['uid1']. "?fields=picture.width(140).height(120),first_name";
-        $requests = file_get_contents($request_url);
-        $data = json_decode($requests);
-        $a = $data->picture->data->url;
-        $_SESSION['uf1'] = $data->first_name;
-        $request_url ="https://graph.facebook.com/" . $_SESSION['uid2']. "?fields=picture.width(140).height(120),first_name";
-        $requests = file_get_contents($request_url);
-        $data = json_decode($requests);
-        $b = $data->picture->data->url;
-        $_SESSION['uf2'] = $data->first_name;
-        $path = 'dummy.png';
-        $originalImage = imagecreatefrompng($path);
-        $u1 = imagecreatefrompng("http://workspace.nazuka.net/sendback.php?l=".$a);
-        $u2 = imagecreatefrompng("http://workspace.nazuka.net/sendback.php?l=".$b);
-        if(imagecopymerge($originalImage, $u1, 45, 70, 0, 0, imagesx($u1), imagesy($u1), 100) && imagecopymerge($originalImage,$u2, 715, 260,0, 0, imagesx($u2), imagesy($u2), 100)){
-            positionText($originalImage, $_SESSION['uf1'], 1);
-            positionText($originalImage, $_SESSION['uf2'], 2);
-            $rand = randomKeyGenerator(5);
-            $img_name = "users/".$_SESSION['uid1']."_".$rand.".png";
-            imagepng($originalImage,$img_name);
-            imagedestroy($originalImage);
-            echo $img_name;
+        try{
+            $request_url ="https://graph.facebook.com/" . $_SESSION['uid1']. "?fields=picture.width(140).height(120),first_name";
+            $requests = file_get_contents($request_url);
+            $data = json_decode($requests);
+            $a = $data->picture->data->url;
+            $_SESSION['uf1'] = $data->first_name;
+            $request_url ="https://graph.facebook.com/" . $_SESSION['uid2']. "?fields=picture.width(140).height(120),first_name";
+            $requests = file_get_contents($request_url);
+            $data = json_decode($requests);
+            $b = $data->picture->data->url;
+            $_SESSION['uf2'] = $data->first_name;
+            $path = 'dummy.png';
+            $originalImage = imagecreatefrompng($path);
+            $u1 = imagecreatefrompng("http://workspace.nazuka.net/sendback.php?l=".$a);
+            $u2 = imagecreatefrompng("http://workspace.nazuka.net/sendback.php?l=".$b);
+            if(imagecopymerge($originalImage, $u1, 45, 70, 0, 0, imagesx($u1), imagesy($u1), 100) && imagecopymerge($originalImage,$u2, 715, 260,0, 0, imagesx($u2), imagesy($u2), 100)){
+                positionText($originalImage, $_SESSION['uf1'], 1);
+                positionText($originalImage, $_SESSION['uf2'], 2);
+                $rand = randomKeyGenerator(5);
+                $img_name = "users/".$_SESSION['uid1']."_".$rand.".png";
+                imagepng($originalImage,$img_name);
+                imagedestroy($originalImage);
+                echo $img_name;
 
-        } else{
-            echo "Can't copy merge";
-            imagedestroy($originalImage);
+            } else{
+                echo "Can't copy merge";
+                imagedestroy($originalImage);
+            }
+        } catch(Exception $e){
+            echo $e;
         }
     }
 
